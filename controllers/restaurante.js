4e1dadefe7a857243a3db361f620dd35e5c16a9e
Arranque1 <Arranque1>
2016-02-17 14:36:49 +0100

    commit clase

--- controllers/restaurante.js
+++ controllers/restaurante.js
@@ -4,39 +4,24 @@ angular.module("restaurante")
 .constant("authUrl", "http://localhost:5500/usuario/login")
 .constant("userUrl", "http://localhost:5500/usuario/me")
 .constant("logOutUrl", "http://localhost:5500/usuario/logout")
-.controller("restauranteCtrl", function ($scope, $http, $location,usersUrl,productosUrl,authUrl,userUrl,logOutUrl) {
+.controller("restauranteCtrl", function ($scope,$window, $http, $location,usersUrl,productosUrl,authUrl,userUrl,logOutUrl) {
 // declaramaos la variable data
 $scope.data = {
 };
 
 // añadir el nuevo usuario a la base de datos
 $scope.addUser = function (userDetails) {
-
-/*$http.get(productosUrl)
+$http.post(usersUrl,userDetails)
 .success(function (data) {
-$scope.data.productos = data;
-$scope.message += $scope.data.productos[0].nombre;
-})
-.error(function (error) {
-$scope.data.error = error;
-});
-*/
-userDetails.tipo = "cliente";
-//if (registerForm.$valid) {
-$http.post(usersUrl,userDetails).success(function (data) {
 $scope.message = "El usuario se ha dado de alta con exito";
-
 })
 .error(function (error) {
-$scope.message = "Error en la insercion";
+$scope.data.errorInsercion = error;
+$window.location.reload();
 });
 
-/*} else {
-$scope.message = "";
-$scope.showValidation = true;
-}*/
 };
-$scope.mensajeLog="Ready";
+
 // autenticacion de usuario
 $scope.authenticate = function (user, pass) {
 $scope.mensajeLog="Ready Ya";
@@ -48,40 +33,41 @@ withCredentials: true
 }).success(function (data) {
 $scope.mensajeLog  = "Exito de login";
 $scope.data.usuarioRegistrado=true;
-//$location.path("/home");
-}).error(function (error) {
+})
+.error(function (error) {
 $scope.authenticationError = error;
 $scope.mensajeLog  = "Error login";
-});
-$http.get(userUrl, {
-withCredentials: true
-}).success(function(data){
+})
+.get(userUrl, {withCredentials: true})
+.success(function(data){
 $scope.data.usuarioActual = data;
+})
+.error(function(error){
+	$scope.data.errorAutorizacion = "Error";
+});
+};
 
-}).error(function(error){$scope.data.usuarioActual = "Error";});
-};
-$scope.existsUser=function(userName){
-  
-};
-// comprueba que existe el usuario y con esta contraseña
+
 
 
 $scope.message = "Ready";
 
-// funcion de error del formulario de entrada datos de nuevo usuario
+// funcion de feedback de error del formulario de entrada datos de nuevo usuario
 $scope.getError = function (error) {
-if (angular.isDefined(error)) {
-if (error.required) {
-return "Please enter a value";
-} else if (error.email) {
-return "Please enter a valid email address";
-}
-}
+	if (angular.isDefined(error)) {
+		if (error.required) {
+				return "Please enter a value";
+		} else if (error.email) {
+		return "Please enter a valid email address";
+		}
+	}
 };
+// funcion de cerrar sesion
 $scope.logOut=function(){
 $http.post(logOutUrl).success(function(data){
-                   $scope.data.usuarioRegistrado = false;
-				   $scope.user={};
+                  $scope.data.usuarioRegistrado = false;
+				  $window.location.reload();
                                });
+
 };
 });
